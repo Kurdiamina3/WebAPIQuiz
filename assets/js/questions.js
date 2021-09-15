33fe866137be59958da28f8f5a220875dafed72a
Kurdiamina3 <kurdiamina3@gmail.com>
2021-09-15 16:05:53 -0400

    WEB API QUIZARONI

---
+++ assets/js/questions.js
@@ -0,0 +1,28 @@
+
+var questions = [
+  {
+    title: "Inside which HTML element do we put the Javascript?"
+    choices: ["<script>","<scripting>","<javascript>","<Js>"]
+    answer: "<script>"
+  }, 
+  {
+    title: "Where is the corrent place to insert a Javascript?"
+    choices:["The <body> section.","The <head> section", "Anywhere", "The very top of the page"]
+    answer: "The <body> section."
+  }, 
+  {
+    title: "How do you create a function in Javascript?"
+    choices:["function myFunction()", "function:myFunction()", "function = myFunction", "FUNCTION MYFUNCION!!"]
+    answer:"function myFunction()"
+  }, 
+  {
+    title: "How do you call a function named 'myFunction'?"
+    choices: ["myFunction()","call function myFunction()", "call myFunction", "Myfunction()"]
+    answer:"myFunction()"
+  }, 
+  {
+    title: "How do you add a comment in Javascript?"
+    choices: ["//This is a comment", "'This is a comment'", "<!--This is a comment-->", "This is a comment."]
+    answer:"//This is a comment"
+  }
+];
